09dbfdc89bc6d3e9ed1e901b5f3ecf213c653159
ESHA SHARMA <112226604+Esha9424@users.noreply.github.com>
2024-10-07 16:03:14 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,11 +1,17 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
 
 
 
